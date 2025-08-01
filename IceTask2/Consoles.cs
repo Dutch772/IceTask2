46f8a1f0312edfbd6cceb12495b34c4e5f50af97
ST10439050 <160716132+Dutch772@users.noreply.github.com>
2025-08-01 14:33:52 +0200

    Add project files.

---
+++ IceTask2/Consoles.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask2
+{
+    public class Consoles
+    {
+        string ConsoleName, ConsoleBrand, ConsolePerformance;
+        int ConsoleAge, ConsolePrice;
+
+        public Consoles(string ConsoleName, int ConsoleAge, string ConsoleBrand, int ConsolePrice, string ConsolePerformance)
+        {
+            this.ConsoleName = ConsoleName;
+            this.ConsoleAge = ConsoleAge;
+            this.ConsoleBrand = ConsoleBrand;
+            this.ConsolePrice = ConsolePrice;
+            this.ConsolePerformance = ConsolePerformance;
+        }
+    public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return this.ConsoleName;
+                else if (index == 1)
+                    return this.ConsoleAge;
+                else if (index == 2)
+                    return this.ConsoleBrand;
+                else if (index == 3)
+                    return this.ConsolePrice;
+                else if (index == 4)
+                    return this.ConsolePerformance;
+                return null;
+            }
+            set
+            {
+                if(index == 0)
+                    this.ConsoleName = (string)value;
+                else if (index == 1)
+                    this.ConsoleAge = (int)value;
+                else if (index == 2)
+                    this.ConsoleBrand = (string)value;
+                else if(index == 3)
+                    this.ConsolePrice = (int)value;
+                else if(index == 4)
+                    this.ConsolePerformance = (string)value;
+            }
+        }
+        public object this[string query]
+        {
+            get
+            {
+                if (query == "name")
+                    return this.ConsoleName;
+                else if (query == "age")
+                    return this.ConsoleAge;
+                else if (query == "brand")
+                    return this.ConsoleBrand;
+                else if (query == "price")
+                    return this.ConsolePrice;
+                else if (query == "performance")
+                    return this.ConsolePerformance;
+                return null;
+            }
+            set
+            {
+                if(query.ToLower().Equals("name"))
+                    this.ConsoleName= (string)value;
+                else if(query.ToLower().Equals("age"))
+                    this.ConsoleAge= (int)value;
+                else if(query.ToLower().Equals("brand"))
+                    this.ConsoleBrand= (string)value;
+                else if (query.ToLower().Equals("price"))
+                    this.ConsolePrice = (int)value;
+                else if (query.ToLower().Equals("performance"))
+                    this.ConsolePerformance = (string)value;
+            }
+        }
+    }
+}
---
+++ Test1/Test1.cs
@@ -0,0 +1,44 @@
+﻿using IceTask2;
+
+namespace Test1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestName()
+        {
+            Consoles console = new Consoles("Series S", 4, "Xbox", 6500, "Good");
+            console[0] = "Playstation 5";
+            Assert.AreEqual(console[0], "Playstation 5");
+        }
+        [TestMethod]
+        public void TesAge()
+        {
+            Consoles console = new Consoles("Series S", 4, "Xbox", 6500, "Good");
+            console[1] = 5;
+            Assert.AreEqual(console["ConsoleAge"], 5);
+        }
+        [TestMethod]
+        public void TestBrand()
+        {
+            Consoles console = new Consoles("Series S", 4, "Xbox", 6500, "Good");
+            console[0] = "Xbox";
+            Assert.AreEqual(console[0], "Xbox");
+        }
+        [TestMethod]
+        public void TestPrice()
+        {
+            Consoles console = new Consoles("Series S", 4, "Xbox", 6500, "Good");
+            console[0] = 6500;
+            Assert.AreEqual(console["ConsolePrice"], 6500);
+        }
+        [TestMethod]
+        public void TestPerformance()
+        {
+            Consoles console = new Consoles("Series S", 4, "Xbox", 6500, "Good");
+            console[0] = "Very Good";
+            Assert.AreEqual(console[0], "Very Good");
+        }
+    }
+}
